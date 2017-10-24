258f541a7b5ce26207912acd706229f2c94b5ea3
Mario Del Real <mdelreal2@carthage.edu>
2017-10-23 21:48:02 -0500

    Added comments to the driver

--- Assignment4/driver.rb
+++ Assignment4/driver.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'sinatra/json'
 require 'sequel'
 
-
+#will be called on start to create a table of records
 configure do
     DB = Sequel.connect('sqlite://Records.db')
 
@@ -19,23 +19,27 @@ end
 
 #handles the initial request for the page
 get '/' do
-    #@retrieved_data = Record.order_by(:score)
+
+    #default display of records sorted by highest to lowest score
     @retrieved_data = Record.reverse_order(:score)
 
     #render the template in the view folders
     erb :welcome
 end
 
+#call to the server that loads the game template
 get '/start_game' do
 
     erb :game
 end
 
+#call to the server that loads the settings template
 get '/settings' do
 
     erb :settings
 end
 
+#call to the server that creates a record from the game
 post '/record' do
 
     records = DB[:records]
@@ -43,12 +47,14 @@ post '/record' do
     records.insert(:name => params[:name], :score => params[:score], :date => DateTime.now)
 end
 
+#call to the server that sends back all of the records from highest score to lowest score and reloads the template
 get '/records_all_time' do
     @retrieved_data = Record.reverse_order(:score)
 
     erb :welcome
 end
 
+#call to the server that sends back all of the records from most recent to oldest date and reloads the template
 get '/records_most_recent' do
 
     @retrieved_data = Record.reverse_order(:date)
@@ -56,6 +62,7 @@ get '/records_most_recent' do
     erb :welcome
 end
 
+#call to the server that sends back all of the records for a single person from highest to lowest score and reloads the template
 get '/records_by_name/:name' do |name|
 
     @retrieved_data = Record.where(:name => name).reverse_order(:score)
